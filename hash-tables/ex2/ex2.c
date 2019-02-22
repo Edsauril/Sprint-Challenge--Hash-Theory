b45d130d458cf3e5e476550ba9d4813e505a1aa7
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-02-22 13:34:44 -0500

    ex2

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,18 +11,29 @@ char **reconstruct_trip(Ticket **tickets, int length)
   char **route = malloc(length * sizeof(char *));
 
   // YOUR CODE HERE
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+  }
 
+  char *destinations = hash_table_retrieve(ht, "NONE");
+  route[0] = destinations;
+
+  for (int i = 1; i < length; i++)
+  {
+    destinations = hash_table_retrieve(ht, destinations);
+    route[i] = destinations;
+  }
   return route;
 }
 
 void print_route(char **route, int length)
 {
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     printf("%s\n", route[i]);
   }
 }
-
-
 
 #ifndef TESTING
 int main(void)
